ab3f377713e6259d57b1c9f3a4f8078159c493e8
VTruong1478 <v.truong1478@gmail.com>
2025-08-23 23:02:16 -0400

    got tailwind working

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working! 🎉
+      </h1>
     </div>
   );
 }
