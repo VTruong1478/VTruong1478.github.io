18e4d86ef27f4e67f907d46c38feb63fa9004da7
VTruong1478 <v.truong1478@gmail.com>
2026-01-15 00:20:34 -0500

    configuring tailwind variables

--- desktop/src/App.jsx
+++ desktop/src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="min-h-screen p-8 bg-bg text-text">
+    <div className="min-h-screen bg-bg text-text p-8">
       <h1 className="text-h1 font-sans">Inter heading</h1>
 
       <p className="mt-4 text-p14 font-pixel">Pixelify Sans body</p>
