a23b321e7f3ff319daf9e3601f0cc09e3fead41f
VTruong1478 <v.truong1478@gmail.com>
2026-02-19 15:39:11 -0500

    connected posthog for analytics

--- desktop/src/main.jsx
+++ desktop/src/main.jsx
@@ -2,10 +2,16 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { PostHogProvider } from "@posthog/react";
 import App from "./App.jsx";
+
+const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY;
+const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <PostHogProvider apiKey={posthogKey} options={{ api_host: posthogHost }}>
+      <App />
+    </PostHogProvider>
+  </StrictMode>,
 );
